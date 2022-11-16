16c5d46ec8e6e4a0705b7f3b6017a09109d4880b
Jung Tae Min <71391013+aquisto535@users.noreply.github.com>
2022-11-16 17:48:24 +0900

    connect to mysql

--- webserver.js
+++ webserver.js
@@ -1,3 +1,14 @@
+//mysql
+// const mysql = require("mysql");
+// const conn = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "123456",
+//   database: "node_dbs",
+// });
+
+// conn.connect();
+
 const express = require("express");
 
 const app = express(); //함수 포인터. 객체로 받음. express의 모든 기능을 담게 됨.
@@ -22,6 +33,15 @@ app.post("/add", (req, res) => {
   console.log(req.body.title);
   console.log(req.body.date);
 
+  // conn.query(
+  //   `insert into todo_bk(title, created) values("${req.body.title}", "${req.body.date}")`,
+
+  //   // [req.body.title, req.body.date],
+  //   function (err, rows, fields) {
+  //     if (err) throw err;
+  //   }
+  // );
+
   //파일 입출력, 엑셀, DB
 
   res.send("전송 완료");
@@ -32,3 +52,5 @@ app.listen(8080, () => {
 });
 
 //nodemon: 코드에 변화가 있을 때마다 서버를 재시작시켜줌.
+
+app.get("/list", (req, res) => {});
